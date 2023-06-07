55a6258af3dec35083655037c1507e7aabe0fa0b
guoyunquan <2782354831@qq.com>
2023-06-07 14:13:19 +0800

    商城商品模块2 提交

---
+++ gulimall_third_party/src/test/java/com/gyq/thirdParty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,72 @@
+package com.gyq.thirdParty;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+@SpringBootTest
+class GulimallThirdPartyApplicationTests {
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+    }
+
+    @Test
+    public void oss() {
+//        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+//        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
+//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "LTAI5tGrkd9eUVVX4KGSEzAY";
+//        String accessKeySecret = "iHGbOqqDhJMlvD1Pd088Yld6qyEO9S";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "gulimall-gyq123";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "cctv.ss.jpg";
+        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
+        String filePath = "/Users/guoyunquan/Pictures/candy-5120x2880-marmellou-jellies-lollipop-delicious-5k-17819.jpg";
+
+        // 创建OSSClient实例。
+
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new File(filePath));
+            // 设置该属性可以返回response。如果不设置，则返回的response为空。
+            putObjectRequest.setProcess("true");
+
+            // 上传文件。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+            // 如果上传成功，则返回200。
+            System.out.println(result.getResponse().getStatusCode());
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
+}
